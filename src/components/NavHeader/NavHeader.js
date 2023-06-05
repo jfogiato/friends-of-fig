462f9b3c87b1b38d099ac2ac087ee12618f0529e
Joe Fogiato <joe.fogiato@gmail.com>
2023-06-05 13:54:22 -0400

    Update: NavBar link to use Link instead of href

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -5,7 +5,7 @@ const NavHeader = () => {
   return (
   <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Friends of Fig</a>
+      <Link to="/" className="navbar-brand">Friends of Fig</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
