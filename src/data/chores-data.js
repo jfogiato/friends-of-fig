db671ac35f2fe817a09500159e28466e69081302
Joe Fogiato <joe.fogiato@gmail.com>
2023-07-05 14:58:56 -0400

    Update: Data with more days and updated property info

--- src/data/chores-data.js
+++ src/data/chores-data.js
@@ -2,13 +2,13 @@ const chores = {
   "data": [
     {
       "id": 1,
-      "name": "AM Food",
+      "name": "AM Dry Food",
       "date": "2023-09-01",
       "completed": false,
     },
     {
       "id": 2,
-      "name": "PM Food",
+      "name": "PM Dry Food",
       "date": "2023-09-01",
       "completed": false,
     },
@@ -22,6 +22,72 @@ const chores = {
       "id": 4,
       "name": "Water",
       "date": "2023-09-01",
+      "completed": false,
+    },
+    {
+      "id": 5,
+      "name": "PM Wet Food",
+      "date": "2023-09-01",
+      "completed": false,
+    },
+    {
+      "id": 6,
+      "name": "AM Dry Food",
+      "date": "2023-09-02",
+      "completed": false,
+    },
+    {
+      "id": 7,
+      "name": "PM Dry Food",
+      "date": "2023-09-02",
+      "completed": false,
+    },
+    {
+      "id": 8,
+      "name": "Litter",
+      "date": "2023-09-02",
+      "completed": false,
+    },
+    {
+      "id": 9,
+      "name": "Water",
+      "date": "2023-09-02",
+      "completed": false,
+    },
+    {
+      "id": 10,
+      "name": "PM Wet Food",
+      "date": "2023-09-02",
+      "completed": false,
+    },
+    {
+      "id": 11,
+      "name": "AM Dry Food",
+      "date": "2023-09-03",
+      "completed": false,
+    },
+    {
+      "id": 12,
+      "name": "PM Dry Food",
+      "date": "2023-09-03",
+      "completed": false,
+    },
+    {
+      "id": 13,
+      "name": "Litter",
+      "date": "2023-09-03",
+      "completed": false,
+    },
+    {
+      "id": 14,
+      "name": "Water",
+      "date": "2023-09-03",
+      "completed": false,
+    },
+    {
+      "id": 15,
+      "name": "PM Wet Food",
+      "date": "2023-09-03",
       "completed": false,
     },
   ]
